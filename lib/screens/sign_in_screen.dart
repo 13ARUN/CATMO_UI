38ee5f026fc698c4cac1cd541e2b1f67247b06c1
13ARUN <13aruneswar@gmail.com>
2025-03-21 15:26:01 +0530

    Add logo asset and update main screen to SignInScreen

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 85, 7, 3),
+            const Color.fromARGB(255, 85, 7, 3),
+            const Color.fromARGB(255, 85, 7, 3),
+            const Color.fromARGB(255, 85, 7, 3),
+            Color.fromARGB(255, 137, 8, 4),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    spacing: 10,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child: Image.asset("assets/logo/logo.png"),
+                      ),
+                      Text(
+                        "CATIE Mobile",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(25),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "Sign In",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30,
+                                  ),
+                                ),
+                                SizedBox(height: 15),
+                                Form(
+                                  child: Column(
+                                    spacing: 8,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Username",
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        cursorColor: Colors.deepOrange,
+                                        decoration: InputDecoration(
+                                          hintText: "Enter Username",
+                                          fillColor: const Color.fromARGB(
+                                            255,
+                                            235,
+                                            239,
+                                            240,
+                                          ),
+                                          filled: true,
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(
+                                              8,
+                                            ),
+                                            borderSide: BorderSide.none,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(height: 6),
+                                      Text(
+                                        "Password",
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        cursorColor: Colors.deepOrange,
+                                        obscureText: true,
+                                        decoration: InputDecoration(
+                                          hintText: "Enter Password",
+                                          fillColor: const Color.fromARGB(
+                                            255,
+                                            235,
+                                            239,
+                                            240,
+                                          ),
+                                          filled: true,
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(
+                                              8,
+                                            ),
+                                            borderSide: BorderSide.none,
+                                          ),
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Checkbox(
+                                            value: false,
+                                            onChanged: (value) {},
+                                            materialTapTargetSize:
+                                                MaterialTapTargetSize
+                                                    .shrinkWrap,
+                                            visualDensity:
+                                                VisualDensity.compact,
+                                          ),
+                                          Text("Show Password"),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text("Forgot your password?"),
+                                ),
+                                SizedBox(height: 8),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text("Remember Me"),
+                                    Checkbox(
+                                      value: false,
+                                      onChanged: (value) {},
+                                      materialTapTargetSize:
+                                          MaterialTapTargetSize.shrinkWrap,
+                                      visualDensity: VisualDensity.compact,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 25),
+                            Row(
+                              spacing: 5,
+                              children: [
+                                SizedBox(
+                                  width: 100,
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      "Cancel",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 45,
+                                  child: VerticalDivider(
+                                    color: Colors.black,
+                                    thickness: 1,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ButtonStyle(
+                                      backgroundColor: WidgetStatePropertyAll(
+                                        const Color.fromARGB(255, 85, 7, 3),
+                                      ),
+                                      foregroundColor: WidgetStatePropertyAll(
+                                        Colors.white,
+                                      ),
+                                      overlayColor: WidgetStatePropertyAll(
+                                        Color.fromARGB(255, 137, 8, 4),
+                                      ),
+                                      shape: WidgetStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            5,
+                                          ),
+                                          side: BorderSide.none,
+                                        ),
+                                      ),
+                                      padding: WidgetStatePropertyAll(
+                                        const EdgeInsets.symmetric(
+                                          horizontal: 16,
+                                          vertical: 12,
+                                        ),
+                                      ),
+                                    ),
+                                    child: const Text(
+                                      "Sign In",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Text(
+                    "v25.03.0 (100)",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 203, 152, 152),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
